64676a69a3d563ce4e9039bad40b01bdb02d6f64
Wells <chawkeye@gmail.com>
2018-01-10 17:07:37 +0800

    user can set timer

--- js/light.js
+++ js/light.js
@@ -1,19 +1,30 @@
 var state = true;
-function togglelight() {
-  var img = document.getElementById("image");
+function toggleLight() {
+  var img = document.getElementById('image');
   state = !state;
-  img.src = state ? "image/on.png" : "image/off.png";
+  img.src = state ? 'image/on.png' : 'image/off.png';
+}
+var timeleft  = 0;
+var setting = 0;
+
+function setTimer() {
+  var time = document.getElementById('time').value;
+  if (isNaN(time)) {
+    alert('請輸入數字');
+  }else {
+    timeleft = time;
+    setting = setInterval(countDownTimer, 1000);
+    document.getElementById('time').value = "";
+  }
 }
 
-var timeleft = 5;
 function countDownTimer(){
-  var img = document.getElementById("image");
-  timeleft -= 1;
+  var img = document.getElementById('image');
+  timeleft -= 1 ;
+  console.log(timeleft);
   if (timeleft <= 0) {
-    img.src = "image/off.png";
+    img.src = 'image/off.png';
     clearInterval(setting);
   }
-  document.getElementById("timer").innerHTML = timeleft;
+  document.getElementById('timer').innerHTML = timeleft;
 }
-
-var setting = setInterval(countDownTimer, 1000);
