eb585087d8fd11b83a2edf68b433211520ee457e
Wells <chawkeye@gmail.com>
2018-01-11 10:53:42 +0800

    add male/female validate

--- js/validate.js
+++ js/validate.js
@@ -6,7 +6,9 @@ function validate() {
     alert("不能全是數字");
   }else if (code.length != 10) {
     alert("長度不對");
-  }else{
-    document.getElementById("result").innerHTML = "驗證通過";
+  }else if (code[1] == "1" ) {
+    document.getElementById("result").innerHTML = "某男驗證通過";
+  }else if (code[1] == "2" ) {
+    document.getElementById("result").innerHTML = "某女驗證通過";
   }
 }
